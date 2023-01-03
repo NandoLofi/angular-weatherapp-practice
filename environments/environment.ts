ae3aa4ed9ffda517c8040275d002faba45e0fc8e
NandoLofi <n.brighter00@gmail.com>
2023-01-02 20:59:49 -0600

    fixing weather app error

--- environments/environment.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-    RapidAPI: "6c087f1dafmshee4c79cbea1744cp1e4819jsncbb07ebc3b6e"
-}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  constructor(private weatherService: WeatherService){
 
-  }
-  ngOnInit(): void {
-    this.weatherService.getWeatherData("San Antonio")
-    .subscribe({
-      next: (response) => 
-      console.log(response)
-    })
-   
-  }
-
-}
