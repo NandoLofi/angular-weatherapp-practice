11bdf8920a4a8be2188658ea3e34d8b8030a9f87
NandoLofi <n.brighter00@gmail.com>
2023-01-03 20:33:25 -0600

    basic change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { WeatherService } from './services/weather.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
+export class AppComponent {
+  title = "weather"
+
+  constructor(private weather: WeatherService){
+    this.weather.getData().subscribe(data=> {
+      console.log(data)
+    })
+  }
+  }
